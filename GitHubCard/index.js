5c242350359786d8913766ec235c8d47a12b0054
Craig Barcinas <cbarcinas@live.com>
2021-01-21 19:10:33 -0800

    created elemets, appended elements, added textContent

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,21 @@
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+axios
+  .get("https://api.github.com/users/cbarcinas")
+  .then((res) => {
+    const user = res.data;
+    console.log(res.data);
+    // after console.logging res, we can see that the user info is located in the data obj so we target that.
+    const userCard = cardMaker(user);
+    // we're passing in user because that's the variable name we gave the res.data
+    cardContainer.appendChild(userCard);
+    // then we append userCard to the cardContainer.
+    //  userCard will run the cardMaker function to create the card and pass in the user variable to give it a username.
+  })
+  .catch((err) => {
+    console.log("we messed up", err);
+  });
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -16,6 +31,7 @@
   STEP 4: Pass the data received from Github into your function,
     and append the returned markup to the DOM as a child of .cards
 */
+const cardContainer = document.querySelector(".cards");
 
 /*
   STEP 5: Now that you have your own card getting added to the DOM, either
@@ -28,7 +44,13 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [
+  "tetondan",
+  "dustinmyers",
+  "justsml",
+  "luishrd",
+  "bigknell",
+];
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
@@ -49,6 +71,41 @@ const followersArray = [];
       </div>
     </div>
 */
+
+function cardMaker(obj) {
+  // create elements
+  const userCard = document.createElement("div");
+  const userImg = document.createElement("img");
+  const cardInfo = document.createElement("div");
+  const userName = document.createElement("h3");
+  const userUserName = document.createElement("p");
+  const userLocation = document.createElement("p");
+  const userProfile = document.createElement("p");
+  const userLink = document.createElement("a");
+  const userFollowers = document.createElement("p");
+  const userFollowing = document.createElement("p");
+  const userBio = document.createElement("p");
+  const linkText = document.createElement("div"); //this is to add text into the a tag
+  // append elements
+  userCard.appendChild(userImg);
+  userCard.appendChild(cardInfo);
+  cardInfo.appendChild(userName);
+  cardInfo.appendChild(userUserName);
+  cardInfo.appendChild(userLocation);
+  cardInfo.appendChild(userProfile);
+  cardInfo.appendChild(userFollowers);
+  cardInfo.appendChild(userFollowing);
+  cardInfo.appendChild(userBio);
+  userProfile.appendChild(userLink);
+  userProfile.appendChild(linkText);
+  // add content to appended elements
+  userName.textContent = obj.name; // this will grab the name from the object that we pass into the function
+  userUserName.textContent = obj.login;
+  userLocation.textContent = `Location: ${obj.location}`;
+  userFollowers.textContent = `Followers: ${obj.followers}`;
+  userFollowing.textContent = `Following: ${obj.following}`;
+  userBio.textContent = `Bio: ${obj.bio}`;
+}
 
 /*
   List of LS Instructors Github username's:
